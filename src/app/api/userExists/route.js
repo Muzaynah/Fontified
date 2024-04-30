f2957b252fe0196901b8762c73e532fbb22b0989
Isra Mansoor <isramansoor9@gmail.com>
2024-04-30 20:40:30 +0500

    Login page, register page

---
+++ src/app/api/userExists/route.js
@@ -0,0 +1,16 @@
+import User from "../../../../models/user";
+import { connect } from "mongoose";
+import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongodb";
+
+export async function POST (req) {
+    try {
+        await connectMongoDB();
+        const {email} = await req.json();
+        const user = await User.findOne({email}).select("_id");
+        console.log("user: ", user);
+        return NextResponse.json({user});
+    } catch (error) {
+        console.log(error);
+    }
+}
